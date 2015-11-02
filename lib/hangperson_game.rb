8870adad3230710cc383715527f8c817ec2330f4
Santiago Medina <devsanlizmed@yahoo.com>
2015-11-02 18:17:20 +0000

    until invalid

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -2,7 +2,22 @@ class HangpersonGame
 
   # add the necessary class methods, attributes, etc. here
   # to make the tests in spec/hangperson_game_spec.rb pass.
-
+  attr_accessor :word
+  attr_accessor :guesses
+  attr_accessor :wrong_guesses
+  
+  def guess(letter)
+    raise ArgumentError if not letter =~ /^[a-zA-Z]$/
+    dletter = letter.downcase
+    return false if (@guesses.include? dletter) || (@wrong_guesses.include? dletter)
+    
+    if @word.include? dletter
+      @guesses += dletter
+    else
+      @wrong_guesses += dletter
+    end
+    return true
+  end
   # Get a word from remote "random word" service
 
   # def initialize()
@@ -10,6 +25,8 @@ class HangpersonGame
   
   def initialize(word)
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
   end
 
   def self.get_random_word
